518b8d742e25abfa5fd5be1d607a95281b06cb55
Fran Gabarda <frangabardaortiz@gmail.com>
2024-12-04 00:31:14 +0100

    Problemas del día 3 resueltos.

---
+++ AdventOfCode/src/aoc2024/dia3/PrimeraEstrella.java
@@ -0,0 +1,47 @@
+package aoc2024.dia3;
+
+import input.AdventOfCodeInputFetcher;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PrimeraEstrella {
+
+    private static final int DIA = 3;
+    private static final int ANYO = 2024;
+
+    public static void main(String[] args) {
+        // Recuperar el input
+        String input = AdventOfCodeInputFetcher.leerInput(DIA, ANYO);
+        System.out.println("*** Input del día "+ DIA +" ***\n" + input);
+        // Resolver el problema
+        int resultado = resolverProblema(input);
+        System.out.println("Resultado: " + resultado);
+
+    }
+
+    private static int resolverProblema(String input) {
+
+        String expresion = "mul\\(\\d{1,3},\\d{1,3}\\)";
+
+        Pattern pattern = Pattern.compile(expresion);
+        Matcher matcher = pattern.matcher(input);
+
+        List<String> apariciones = new ArrayList<>();
+
+        while (matcher.find()){
+            apariciones.add(matcher.group());
+        }
+
+        int resultado = 0;
+
+        for (String aparicion : apariciones) {
+            String[] numeros = aparicion.substring(4, aparicion.length() - 1).split(",");
+            resultado += Integer.parseInt(numeros[0]) * Integer.parseInt(numeros[1]);
+        }
+
+        return resultado;
+    }
+}
